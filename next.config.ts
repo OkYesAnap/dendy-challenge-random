4e8d6783163eefa05c0acf3946de6fb26a6af3de
Panaseiko Andrii <okyesanap@gmail.com>
2025-03-13 20:54:56 +0200

    all

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,8 @@ const isProduction = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   output: 'export',
   reactStrictMode: true,
-  basePath: isProduction ? '/dendy-challenge-random' : '',
-  assetPrefix: isProduction ? '/dendy-challenge-random/' : '',
+  basePath: '/dendy-challenge-random',
+  // assetPrefix: isProduction ? '/dendy-challenge-random/' : '',
   images:{
     domains: ['localhost'],
     unoptimized: true,
