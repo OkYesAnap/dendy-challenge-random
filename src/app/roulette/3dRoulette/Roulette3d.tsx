7ca0da06af51497c99f958abccdfd95ad56d3c38
okyesnap <okyesanap@gmail.com>
2025-12-03 01:35:27 +0200

    Fix incorrect slot identify.

--- src/app/roulette/3dRoulette/Roulette3d.tsx
+++ src/app/roulette/3dRoulette/Roulette3d.tsx
@@ -48,7 +48,7 @@ const Roulette3d: React.FC<Roulette3dProps> = ({isOpen, onClose}) => {
                         <SquareButton
                             icon={"!!!"}
                             onClickButton={() => {
-                                dispatch(increaseDecreaseRotationSpeed(0.2));
+                                dispatch(increaseDecreaseRotationSpeed(5));
                             }}
                         />
                     </div>
