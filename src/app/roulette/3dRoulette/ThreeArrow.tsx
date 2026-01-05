3041db19d141a4cd574c53d0f9219cd1c3b8431a
Andrii Panaseiko <okyesanap@gmail.com>
2026-01-05 04:08:36 +0200

    Hot fix routes.

--- src/app/roulette/3dRoulette/ThreeArrow.tsx
+++ src/app/roulette/3dRoulette/ThreeArrow.tsx
@@ -16,10 +16,12 @@ import {
 import {calcRadius} from "@/app/roulette/3dRoulette/utils";
 import {finalSpeed} from "@/app/roulette/3dRoulette/threeConstants";
 import {useGLTF} from "@react-three/drei";
+import {isDev} from "@/constants/audioEnv";
 
+const url = isDev ? '/models/dendy.glb' : '/dendy-challenge-random/models/dendy.glb'
 
 function DendyModel() {
-    const { scene } = useGLTF('/models/dendy.glb');
+    const { scene } = useGLTF(url);
     return <primitive position={[-0.1, 1.27, 0.2]} scale={0.3} object={scene} />;
 }
 
--- src/constants/audioEnv.ts
+++ src/constants/audioEnv.ts
@@ -1,4 +1,4 @@
-const isDev = process.env.NODE_ENV === 'development';
+export const isDev = process.env.NODE_ENV === 'development';
 export const audioPath = isDev ? '/audio/' : '/dendy-challenge-random/audio/';
 export const audioSrcNames = [
     '01-Title.mp3',
